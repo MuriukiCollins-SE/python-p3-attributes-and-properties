13ac4d26806b813ced9d4894522f4d9eaa773ebc
collins muriuki <muriukicollins48@gmail.com>
2025-05-17 17:28:34 +0300

    final commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        if breed is not None and breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        elif not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed is not None and breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            self.breed = None
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,22 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        # Always check name first, but only if name is provided
+        if name is not None and (not isinstance(name, str) or len(name) == 0 or len(name) > 25):
+            print("Name must be string between 1 and 25 characters.")
+            self.name = None
+            self.job = None
+        elif job is not None and job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self.name = None
+            self.job = None
+        else:
+            if name is not None:
+                self.name = name.title()
+            else:
+                self.name = None
+            if job is not None and job in APPROVED_JOBS:
+                self.job = job
+            else:
+                self.job = None
